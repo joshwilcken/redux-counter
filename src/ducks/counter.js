2ba08676a0a63de7fc2f4dc6b3ab180fc330a810
joshwilcken <joshwilcken@gmail.com>
2017-08-29 16:11:27 -0600

    committing completed project

--- src/ducks/counter.js
+++ src/ducks/counter.js
@@ -1,2 +1,31 @@
 // change this to true to see test results on the black diamond section.
-export const BLACK_DIAMOND = false;
+const INCREMENT = 'INCREMENT'
+const DECREMENT = 'DECRMENT'
+
+const initialState = {
+    currentValue: 0
+}
+export default function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT:
+            return { currentValue: state.currentValue + action.amount}
+        case DECREMENT: 
+            return { currentValue: state.currentValue - action.amount}
+        default:
+            return state
+    }
+}
+export function increment(amount){
+    return {
+        amount,
+        type: INCREMENT
+    }
+}
+export function decrement(amount){
+    return {
+        amount,
+        type: DECREMENT
+    }
+}
+
+
